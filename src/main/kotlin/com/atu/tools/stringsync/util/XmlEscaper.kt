4a5e6f426f9a3f1bad7a6273fbe41fc5ec403068
HoangTV <hoang.than@tp.com.vn>
2026-03-31 15:53:25 +0700

    update case \'

--- src/main/kotlin/com/atu/tools/stringsync/util/XmlEscaper.kt
+++ src/main/kotlin/com/atu/tools/stringsync/util/XmlEscaper.kt
@@ -7,7 +7,27 @@ object XmlEscaper {
             .replace("<", "&lt;")
             .replace(">", "&gt;")
             .replace("\"", "&quot;")
-            .replace("'", "\\'")
+            .let(::escapeUnescapedApostrophes)
+    }
+
+    private fun escapeUnescapedApostrophes(value: String): String {
+        val out = StringBuilder(value.length + 8)
+        for (i in value.indices) {
+            val c = value[i]
+            if (c == '\'') {
+                var backslashes = 0
+                var j = i - 1
+                while (j >= 0 && value[j] == '\\') {
+                    backslashes++
+                    j--
+                }
+                if (backslashes % 2 == 0) {
+                    out.append('\\')
+                }
+            }
+            out.append(c)
+        }
+        return out.toString()
     }
 
     fun hasInjectionPattern(value: String): Boolean {
